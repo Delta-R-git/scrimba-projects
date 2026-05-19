c24276bc0771a948b604f6b6b3e9924983a3694a
Delta-R-git <rishalmohammed0k@gmail.com>
2026-05-20 00:23:26 +0530

    Add files via upload

---
+++ Score-Board/index.js
@@ -0,0 +1,36 @@
+let hm = document.getElementById(home)
+let gst = document.getElementById(guest)
+
+// HOME
+
+function hm1(){
+    hm += 1
+    home.innerText = hm
+}
+
+function hm2(){
+    hm += 2
+    home.innerText = hm
+}
+
+function hm3(){
+    hm += 3
+    home.innerText = hm
+}
+
+// GUEST
+
+function gst1(){
+    gst += 1
+    guest.innerText = gst
+}
+
+function gst2(){
+    gst += 2
+    guest.innerText = gst
+}
+
+function gst3(){
+    gst += 3
+    guest.innerText = gst
+}
